e7410b6d72c8b07b6527b15439b2ed3928b31218
Yara <yara.proenca.28@gamil.com>
2021-08-14 00:43:15 -0300

    Estrutura function tomHollandSpider

---
+++ global.js
@@ -0,0 +1,5 @@
+var name = prompt("Digite seu nome:");
+var caracter1 = 'Peter Parker';
+var caractr2= 'Miles Morales';
+var caracter3= 'Peter Parker';
+
--- js/levelOneHollandJS.js
+++ js/levelOneHollandJS.js
@@ -1,42 +1,45 @@
-// hollandSpider();
-// function hollandSpider(){
-//     var valor = parseInt(
-//         prompt(
-//             "blablabla: \n"  + "1 blabal \n" + " 2 blalala"
-//         )
+
+
+function tomHollandSpider(){
+
+    var hollandSpider = parseInt( prompt( 'blalalala: \n' + '1 - blalala \n' + '2 - blalala'));
+
+    while(true){
+
+        hollandSpider = parseInt( prompt( 'blalalala: \n' + '1 - blalala \n' + '2 - blalala'));      
+
+        if(hollandSpider === 1) {
+            
+            alert('SE DEU MAU! Volte ao início');
+
+           return document.write(
+                `<body class="bgTomHolland">
+                    <a href="/index.html">
+                        <button>início</button>
+                    </a>
+                <body>`
+            );
+            break
+        }
         
-//     );
-// }
-
-function hollandSpider(){
-    
+        else if(hollandSpider === 2) {
+           return document.write(
+               `<body class="bgTomHolland">
+                    <a href="/levelsTomHolland_HTML/levelTwoHolland.html">
+                            <button> next </button>
+                    </a>
+                <body>`   
+              
+            );
+            break
+        }
+    }
 }
 
 
+tomHollandSpider();
 
 
-var hollandSpider = parseInt( prompt( 'blalalala: \n' + '1 - blalala \n' + '2 - blalala'));
 
 
 
-if(hollandSpider === 1) {
-    alert('SE DEU MAU! Volte ao início');
-    document.write(
-        `<body class="bgTomHolland">
-            <a href="/index.html">
-                <button>início</button>
-            </a>
-        <body>`
-    );
-}
-
-else{
-    document.write(
-       `<body class="bgTomHolland">
-            <a href="/levelsTomHolland_HTML/levelTwoHolland.html">
-                    <button> next </button>
-            </a>
-        <body>`   
-      
-    );
-}
